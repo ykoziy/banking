354029ab94a31b65b1b714f46566ed357215b0b2
Yuriy Koziy <yukoziy@gmail.com>
2022-09-25 13:38:10 -0400

    Added UserRepository and AddressRepository

---
+++ src/main/java/com/ykz/banking/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.ykz.banking.repository;
+
+import com.ykz.banking.model.Address;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AddressRepository extends CrudRepository<Address, Long> {
+}
