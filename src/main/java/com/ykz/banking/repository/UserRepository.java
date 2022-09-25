5c4bdea26909198821722d3f3f85c026a3b49264
Yuriy Koziy <yukoziy@gmail.com>
2022-09-25 13:41:40 -0400

    User repository now uses Long for ID

--- src/main/java/com/ykz/banking/repository/UserRepository.java
+++ src/main/java/com/ykz/banking/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.ykz.banking.repository;
 import com.ykz.banking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findById(Long id);
 }
