b88b0b8c71df650cbd5ca98085c1edd0357c22fc
Yuriy Koziy <yukoziy@gmail.com>
2022-09-25 18:52:49 -0400

    Added service methods to get by email and username

--- src/main/java/com/ykz/banking/service/UserService.java
+++ src/main/java/com/ykz/banking/service/UserService.java
@@ -4,6 +4,9 @@ import com.ykz.banking.exception.UserNotFoundException;
 import com.ykz.banking.model.User;
 import com.ykz.banking.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +19,17 @@ public class UserService {
 
     public User getById(Long id) throws UserNotFoundException {
         return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User Not Found"));
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public Optional<User> getByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
