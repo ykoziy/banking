ab39e0bf9084b6fbdc8eb200395e0d5307df72b7
Yuriy Koziy <yukoziy@gmail.com>
2022-09-25 13:57:00 -0400

    fixed user repository

--- src/main/java/com/ykz/banking/repository/UserRepository.java
+++ src/main/java/com/ykz/banking/repository/UserRepository.java
@@ -4,5 +4,4 @@ import com.ykz.banking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(Long id);
 }
